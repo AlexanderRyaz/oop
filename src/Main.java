c93170978d0d78936d21838ec7917650615880cb
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-09-23 15:08:58 +0300

    создали класс и обьекты

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,20 @@ public class Main {
         human1.setTown("Минск");
         human1.setYearOfBirth(35);
         System.out.println(human1);
+        Human human2 = new Human();
+        human2.setName("Аня");
+        human2.setTown("Москва");
+        human2.setYearOfBirth(29);
+        System.out.println(human2);
+        Human human3 = new Human();
+        human3.setName("Катя");
+        human3.setTown("Калининград");
+        human3.setYearOfBirth(28);
+        System.out.println(human3);
+        Human human4 = new Human();
+        human4.setName("Артем");
+        human4.setTown("Москва");
+        human4.setYearOfBirth(27);
+        System.out.println(human4);
     }
 }
